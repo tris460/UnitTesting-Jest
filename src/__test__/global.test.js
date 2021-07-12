9e89d234e032289828f8fce031ef70bda9d1865e
Bety Martinez <trism460@gmail.com>
2021-07-12 11:23:38 -0500

    Estructura para probar si un núm es mayor

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,7 @@
 const text="Hola Mundo";
 const fruits=['manzana','melon','sandia','uva'];
+let n1=28;
+let n2=20;
 
 test('Debe contener un texto', ()=>{
     expect(text).toMatch(/Mundo/);
@@ -7,4 +9,8 @@ test('Debe contener un texto', ()=>{
 
 test('El array contiene: melon',()=>{
     expect(fruits).toContain('melon');
+})
+
+test('N1 es mayor que N2',()=>{
+    expect(n1).toBeGreaterThan(n2);
 })
