20d490281964fb83ffd7356cabf7b98e4e858920
Bety Martinez <trism460@gmail.com>
2021-07-12 11:21:45 -0500

    Estructura para probar arrays

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,10 @@
 const text="Hola Mundo";
+const fruits=['manzana','melon','sandia','uva'];
 
 test('Debe contener un texto', ()=>{
     expect(text).toMatch(/Mundo/);
+})
+
+test('El array contiene: melon',()=>{
+    expect(fruits).toContain('melon');
 })
