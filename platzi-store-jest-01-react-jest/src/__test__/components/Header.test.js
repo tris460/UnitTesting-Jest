a0dbab1a0ae8eca374dccc724df88b0a31a5e824
Beatriz Martinez <trism@gmail.com>
2021-07-16 01:09:12 -0500

    Render del componente Header

---
+++ platzi-store-jest-01-react-jest/src/__test__/components/Header.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount, shallow} from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header'
+
+describe('<Header />', () => {
+    test('Render del componente Header',() => {
+        const header = shallow(
+            <ProviderMock>
+                <Header />
+            </ProviderMock>
+        );
+        expect(header.length).toEqual(1);
+    });
+})
