c3d38aa7dd1ec1a1e98f91bca16aba0431932922
Beatriz Martinez <trism@gmail.com>
2021-07-17 18:15:46 -0500

    Reducer - ADD_TO_CART

--- platzi-store-jest-01-react-jest/src/__test__/reducers/reducer.test.js
+++ platzi-store-jest-01-react-jest/src/__test__/reducers/reducer.test.js
@@ -4,5 +4,21 @@ import ProductMock from '../../__mocks__/ProductMock';
 describe('Reducers',()=>{
     test('Retornar initialState',()=>{
         expect(reducer({},{})).toEqual({});
+    });
+    test('ADD_TO_CART',()=>{
+        const initialState = {
+            cart: [],
+        };
+        const payload = ProductMock;
+        const action = {
+            type: 'ADD_TO_CART',
+            payload,
+        };
+        const expected = {
+            cart: [
+                ProductMock,
+            ],
+        };
+        expect(reducer(initialState, action)).toEqual(expected);
     })
 })
