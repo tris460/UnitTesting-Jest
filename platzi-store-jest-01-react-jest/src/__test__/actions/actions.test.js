fc21391b9d9c3e59e1e101b606c389f4039b08ac
Beatriz Martinez <trism@gmail.com>
2021-07-17 17:54:35 -0500

    Actions - removeFromCart

---
+++ platzi-store-jest-01-react-jest/src/__test__/actions/actions.test.js
@@ -0,0 +1,20 @@
+import actions from '../../actions/';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions',()=>{
+    const payload=ProductMock;
+    test('addToCart',()=>{
+        const expected = {
+            type: "ADD_TO_CART",
+            payload,
+        };
+        expect(actions.addToCart(payload)).toEqual(expected);
+    });
+    test('removeFromCart',()=>{
+        const expected={
+            type: "REMOVE_FROM_CART",
+            payload,
+        };
+        expect(actions.removeFromCart(payload)).toEqual(expected);
+    })
+})
